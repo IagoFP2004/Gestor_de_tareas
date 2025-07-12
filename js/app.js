2ace3462c33cc9406cdca14fbbf3cfe827aed93a
IagoFP <iagofranciscoperez123@gmail.com>
2025-07-13 01:39:26 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var inputTarea = document.getElementById("nuevaTarea");
+var botonAnhadir = document.getElementById("anhadirTarea");
+
+botonAnhadir.addEventListener("click", function() {
+   alert(inputTarea.value);
+}) 
