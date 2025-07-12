8dd4c2420482ae2d151830307f91deb4a508c645
IagoFP <iagofranciscoperez123@gmail.com>
2025-07-13 01:54:35 +0200

    Se añaden diferentes tareas a la lista

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 var inputTarea = document.getElementById("nuevaTarea");
 var botonAnhadir = document.getElementById("anhadirTarea");
+var tareas = [];
 
 botonAnhadir.addEventListener("click", function() {
-   alert(inputTarea.value);
+   var nuevaTarea = inputTarea.value.trim();
+   if (nuevaTarea !== "" && !tareas.includes(nuevaTarea)) {
+      tareas.push(nuevaTarea);
+      inputTarea.value = ""; // Limpiar el campo de entrada
+      mostrarTareas(tareas); // Mostrar las tareas actualizadas
+   } else {
+      inputTarea.value = ""; // Limpiar el campo de entrada
+   }
 }) 
+
+function mostrarTareas(tareas) {
+   var lista = document.getElementById("listaTareas");
+   lista.innerHTML = ""; // Limpiar la lista antes de mostrar
+   tareas.forEach((tarea)=> {
+      var li = document.createElement("li");
+      li.textContent = tarea;
+      lista.appendChild(li);
+   })
+}
