729a3401519a1b094bdd75ed0844d31cdd941c33
IagoFP <iagofranciscoperez123@gmail.com>
2025-07-13 02:18:19 +0200

    Al añadir tareas te permite seleccionar las completadas y te cuenta el numero de tareas

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var inputTarea = document.getElementById("nuevaTarea");
 var botonAnhadir = document.getElementById("anhadirTarea");
+var contadorTareas = document.getElementById("numeroTareas");
 var tareas = [];
 
 botonAnhadir.addEventListener("click", function() {
@@ -18,7 +19,29 @@ function mostrarTareas(tareas) {
    lista.innerHTML = ""; // Limpiar la lista antes de mostrar
    tareas.forEach((tarea)=> {
       var li = document.createElement("li");
+      var checkbox = document.createElement("input");
+
       li.textContent = tarea;
+      checkbox.type = "checkbox";
+      checkbox.addEventListener("click", function() {
+         if (checkbox.checked) {
+            li.style.textDecoration = "line-through"; // Marcar como completada
+            li.style.backgroundColor = "lightgreen"; // Cambiar el color de fondo
+            li.style.color = "black"; // Cambiar el color del texto
+         } else {
+            li.style.textDecoration = "none"; // Desmarcar
+            li.style.backgroundColor = ""; // Cambiar el color de fondo
+            li.style.color = "white"; // Cambiar el color del texto
+         }
+      });
       lista.appendChild(li);
+      li.appendChild(checkbox);
+      actualizarContador();
    })
 }
+
+function actualizarContador() {
+   contadorTareas.textContent = "Tareas Pendientes: " + tareas.length;
+}
+
+actualizarContador(); // Inicializar el contador de tareas al cargar la página
